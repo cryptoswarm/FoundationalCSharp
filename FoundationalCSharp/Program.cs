7f8f6b2757f46f8e213589a3cdd28b90a4a4ab45
cryptoswarm <mokhtar.safir@outlook.com>
2024-02-05 21:36:52 -0500

    Store and retrieve data using literal and variable values in C#

--- FoundationalCSharp/Program.cs
+++ FoundationalCSharp/Program.cs
@@ -1,10 +1,37 @@
-﻿//Console.WriteLine("Hello World!");
-// Console.Write("Congratulations!");
-// Console.Write(" ");
-// Console.Write("You wrote your first lines of code.");
+﻿// Store and Retrieve Data Using Literal and Variable Values in C#
+// Console.WriteLine('a');
+// Console.WriteLine(777);
+// Console.WriteLine(0.33f);
+// Console.WriteLine(2.62);
+// Console.WriteLine(2.6262626262626262626262626262m);
 
+//declare variables
 
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
+// string firstName;
+// firstName = "Bob";
+// Console.WriteLine(firstName);
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+// firstName = "Isabella";
+// Console.WriteLine(firstName);
+// firstName = "Yasmin";
+// Console.WriteLine(firstName);
 
-// first lines of code
+//declare implicitly typed local variables
+// var message = "Hello World!";
+// message = 10.703m;
+
+//Quiz
+// var firstName = "Bob";
+// var messages = 3;
+// var temperature = 34.4f;
+// Console.Write("Hello, ");
+// Console.Write(firstName);
+// Console.Write("! You have ");
+// Console.Write(messages);
+// Console.Write(" messages in your inbox. The temperature is ");
+// Console.Write(temperature);
+// Console.Write(" celsius.");
+
+Console.WriteLine(34.40m);
+
--- README.md
+++ README.md
@@ -17,3 +17,29 @@
 - [ ] The L in WriteLine should be lower-case.
 - [x] It's missing a semi-colon at the end
 - [ ] The string should use single-quotes.
+
+# Store and retrieve data using literal and variable values in C#
+
+1. Which of the following variable names should or must be avoided?
+
+- [x] $DATA
+- [ ] registrationComplete
+- [ ] flag
+
+2. What is the problem with this line of code: var message;
+
+- [ ] var isn't a data type.
+- [x] Use of var keyword without initializing the variable
+- [ ] A variable named message should always be a string.
+
+3. Which of the following is the output of Console.WriteLine(34.40M);?
+
+- [ ] 34.40M
+- [ ] 34.4
+- [x] 34.40
+
+4. Which of the following lines of code creates a variable correctly?
+
+- [ ] nt x = 12.3m;
+- [x] decimal x = 12.3m;
+- [ ] bool x = 'False';
