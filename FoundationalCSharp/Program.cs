c97a026829a9fdf08063f6b64e778a7b57fdb213
cryptoswarm <mokhtar.safir@outlook.com>
2024-02-11 17:37:57 -0500

    basic string formatting

--- FoundationalCSharp/Program.cs
+++ FoundationalCSharp/Program.cs
@@ -1,37 +1,73 @@
-﻿// Store and Retrieve Data Using Literal and Variable Values in C#
-// Console.WriteLine('a');
-// Console.WriteLine(777);
-// Console.WriteLine(0.33f);
-// Console.WriteLine(2.62);
-// Console.WriteLine(2.6262626262626262626262626262m);
+﻿
+using System.Text;
+//basic string formatting
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
 
-//declare variables
+Console.WriteLine("Hello \"World!\"");
 
-// string firstName;
-// firstName = "Bob";
-// Console.WriteLine(firstName);
-// firstName = "Liem";
-// Console.WriteLine(firstName);
-// firstName = "Isabella";
-// Console.WriteLine(firstName);
-// firstName = "Yasmin";
-// Console.WriteLine(firstName);
+Console.WriteLine("c:\\source\\repos");
 
-//declare implicitly typed local variables
-// var message = "Hello World!";
-// message = 10.703m;
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
 
-//Quiz
-// var firstName = "Bob";
-// var messages = 3;
-// var temperature = 34.4f;
-// Console.Write("Hello, ");
-// Console.Write(firstName);
-// Console.Write("! You have ");
-// Console.Write(messages);
-// Console.Write(" messages in your inbox. The temperature is ");
-// Console.Write(temperature);
-// Console.Write(" celsius.");
+//Verbatism string
 
-Console.WriteLine(34.40m);
+Console.WriteLine(@"Generating invoices for customer ""Contoso Corp"" ...
+
+Invoice: 1021           Complete!
+Invoice: 1022           Complete!
+
+Output Directory:   ");
+
+
+// Unicode escape characters
+//using the \u escape sequence, then a four-character code representing some character in Unicode (UTF-16).
+// Console.WriteLine("\u3053");
+
+//// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// // User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//String concatenation
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+
+////Or better
+
+
+firstName = "Bob";
+message = new StringBuilder("Hello ").Append(firstName).ToString();
+Console.WriteLine(message);
+
+firstName = "Foo";
+string greeting = "Hello";
+message = greeting + " " + firstName + "!";
+Console.WriteLine(message);
+
+//Or better
+
+Console.WriteLine(new StringBuilder().Append("Hello ").Append("Bob!").ToString());
+
+//// Challenge: Format and display instructions
+
+/**
+// Desired output
+// View English output:
+//   c:\Exercise\ACME\data.txt
+
+// Посмотреть русский вывод:
+//   c:\Exercise\ACME\ru-RU\data.txt
+*/
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+Console.WriteLine($"View English output:\n  c:\\Exercise\\{projectName}\\data.txt");
+Console.WriteLine($"{russianMessage}:\n  c:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+
+
 
